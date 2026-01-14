eb0aff8085a04b91871682965db59014332f9784
barsyildiz <yildizbars51@gmail.com>
2026-01-14 19:33:52 +0300

    functions.php güncellendi

--- functions.php
+++ functions.php
@@ -1,36 +1,43 @@
 <?php
 
+$dosya = "notlar.txt";
+
+// Dosya yoksa otomatik oluştur
+if (!file_exists($dosya)) {
+    file_put_contents($dosya, "");
+}
+
 function notEkle($not)
 {
+    global $dosya;
+
     $not = strip_tags(trim($not));
     $tarih = date("d.m.Y H:i");
-    file_put_contents("notlar.txt", "$tarih - $not" . PHP_EOL, FILE_APPEND);
+
+    file_put_contents($dosya, "$tarih - $not" . PHP_EOL, FILE_APPEND);
 }
 
 function notlariGetir()
 {
-    if (!file_exists("notlar.txt")) {
-        return [];
-    }
+    global $dosya;
 
-    return array_reverse(file("notlar.txt", FILE_IGNORE_NEW_LINES));
+    return array_reverse(file($dosya, FILE_IGNORE_NEW_LINES));
 }
 
 function notSil($index)
 {
-    if (!file_exists("notlar.txt")) {
-        return;
-    }
+    global $dosya;
 
-    $notlar = file("notlar.txt", FILE_IGNORE_NEW_LINES);
+    $notlar = file($dosya, FILE_IGNORE_NEW_LINES);
 
-    // index düzeltmesi
+    // index düzeltmesi (ters liste)
     $gercekIndex = count($notlar) - 1 - $index;
 
     if (isset($notlar[$gercekIndex])) {
         unset($notlar[$gercekIndex]);
+
         file_put_contents(
-            "notlar.txt",
+            $dosya,
             implode(PHP_EOL, $notlar) . PHP_EOL
         );
     }
